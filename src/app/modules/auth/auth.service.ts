0cb2b764fc56af228fce7fb6e67bf4b807365f13
Mostary Jahan <155467590+mostaryjahan@users.noreply.github.com>
2026-09-03 20:20:54 +0600

    fix: remove unused credentialsLogin function and imports from auth.service.ts

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,5 +1,4 @@
 import { JwtPayload } from "jsonwebtoken";
-import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import bcryptjs from "bcryptjs";
 import httpStatus from "http-status-codes";
@@ -7,36 +6,7 @@ import { envVars } from "../../config/env";
 import AppError from "../../errorHelpers/AppError";
 import {
   createNewAccessTokenWithRefreshToken,
-  createUserTokens,
 } from "../../utils/userTokens";
-
-const credentialsLogin = async (payload: Partial<IUser>) => {
-  const { email, password } = payload;
-  const isUserExist = await User.findOne({ email });
-
-  if (!isUserExist) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist");
-  }
-  const isPasswordMatched = await bcryptjs.compare(
-    password as string,
-    isUserExist.password as string
-  );
-
-  if (!isPasswordMatched) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Incorrect password");
-  }
-
-  const userTokens = createUserTokens(isUserExist);
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { password: pass, ...rest } = isUserExist.toObject();
-
-  return {
-    accessToken: userTokens.accessToken,
-    refreshToken: userTokens.refreshToken,
-    user: rest,
-  };
-};
 
 // get new token
 const getNewAccessToken = async (refreshToken: string) => {
@@ -72,7 +42,6 @@ const resetPassword = async (
 };
 
 export const AuthServices = {
-  credentialsLogin,
   getNewAccessToken,
   resetPassword,
 };
